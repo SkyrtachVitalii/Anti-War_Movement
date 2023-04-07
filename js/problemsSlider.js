4fac3256fd08265c57a1322f6acc1b73788ec831
Skyrtach Vitalii <vitaliiskirtac@gmail.com>
2023-04-07 15:48:50 +0300

    first commit

--- js/problemsSlider.js
+++ js/problemsSlider.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){
     $('.problemsSlider').slick({
-        infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         speed: 1000,
         arrows: true,
-        centerMode: true,
+        variableWidth: true,
         autoplay: true,
+        infinite: true,
+        centerMode: true,
+        dots: true,
         prevArrow: "<img src='media/icons/arrow-left.png' class='slick-arrows slick-prev' alt='1'>",
-        nextArrow: "<img src='media/icons/arrow-right.png' class='slick-arrow slick-next' alt='2'>"
+        nextArrow: "<img src='media/icons/arrow-right.png' class='slick-arrow slick-next' alt='2'>",
+        responsive: [
+          {
+            breakpoint: 1100,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              slidesToShow: 2,
+            }
+          },
+          {
+            breakpoint: 860,
+            settings: {
+              slidesToShow: 1,
+              centerMode: true,
+              arrows: false
+            }
+          }
+        ]
     });
   });
